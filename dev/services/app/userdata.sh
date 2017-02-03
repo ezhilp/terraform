288ae9fb2f51b98b50067b12fc990ddf367626bd
root <root@ip-10-0-3-157.ec2.internal>
2017-02-03 18:02:45 +0000

    added elb

--- dev/services/app/userdata.sh
+++ dev/services/app/userdata.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -v
 
+sudo apt-get update -y
+
+sudo apt-get install -y nginx > /tmp/nginx.log
+
 sudo apt-get install unzip
+
 sudo mkdir -p /opt/terraform
 sudo cd /opt/terraform
 sudo wget https://releases.hashicorp.com/terraform/0.6.6/terraform_0.6.6_linux_amd64.zip
@@ -10,6 +15,7 @@ sudo mv ~/.bashrc.1 ~/.bashrc | sed '4 i\PATH=$PATH:/opt/terraform' ~/.bashrc > 
 sudo mv ~/.bashrc.1 ~/.bashrc | sed '6 i\alias tfm=terraform' ~/.bashrc > ~/.bashrc.1
 sudo source ~/.bashrc
 
-sudo cd /mnt
+sudo apt install awscli -y
 
+sudo cd /mnt
 sudo git clone https://github.com/ezhilp/terraform.git > out.log 2>&1
