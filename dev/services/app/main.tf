119dfeac4120d333e7d86c094125f7e74db87e35
root <root@ip-10-0-3-157.ec2.internal>
2017-02-03 15:00:47 +0000

    modified files

--- dev/services/app/main.tf
+++ dev/services/app/main.tf
@@ -1,12 +1,37 @@
 provider "aws" {}
 
-resource "aws_instance" sample {
-	ami = "{var.ami}"
-	subnet_id = "{var.subnet}"
-	instance_type = "t2.micro"
-	key_name = "{var.key}"
+resource "aws_security_group" "security_allow_all" {
+	vpc_id = "${var.vpc}"
+	name = "BMSTest"
+	description = "BMSTest"
+  ingress {
+      from_port = 0
+      to_port = 65535
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+ }
 }
 
-output "sample" {
+resource "aws_instance" sample {
+	ami = "${var.ami}"
+	subnet_id = "${var.subnet}"
+	instance_type = "t2.micro"
+	key_name = "${var.key}"
+	security_groups = ["${aws_security_group.security_allow_all.id}"]
+	tags = { Name = "Test" }
+	user_data = "${file("userdata.sh")}"
+}
+
+output "instance_id" {
 	value = "${aws_instance.sample.id}"
+}
+
+output "security_group_id" {
+	value = "${aws_security_group.security_allow_all.id}"
 }
